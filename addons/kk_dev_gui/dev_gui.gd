811df6b3b73498d90b8df9e8d5a8c750e91a9e70
Tobias Mollstam <tobias@mollstam.com>
2023-10-26 21:20:13 +0200

    Added initial support for buttons :high_brightness:

--- addons/kk_dev_gui/dev_gui.gd
+++ addons/kk_dev_gui/dev_gui.gd
@@ -5,18 +5,33 @@ var _labels: Dictionary = {}
 var _labels_reuse: Dictionary = {}
 var _labels_next_seq_id: int = 0
 
+var _buttons: Dictionary = {}
+var _buttons_reuse: Dictionary = {}
+var _buttons_clicked: Dictionary = {}
+var _buttons_clicked_prev: Dictionary = {}
+var _buttons_next_seq_id: int = 0
+
 static var _instance: DevGUI
 
 static func label(text: String) -> void:
 	if (_instance == null):
 		return
-	var id = str(_instance._labels_next_seq_id)
+	var id = str("label_", _instance._labels_next_seq_id)
 	_instance._labels_next_seq_id = _instance._labels_next_seq_id + 1
 	var l = _instance._get_or_create_label(id)
 	l.text = text
 
+static func button(text: String) -> bool:
+	if (_instance == null):
+		return false
+	var id = str("button_", _instance._buttons_next_seq_id)
+	_instance._buttons_next_seq_id = _instance._buttons_next_seq_id + 1
+	var b = _instance._get_or_create_button(id)
+	b.text = text
+	return _instance._buttons_clicked_prev.has(id)
+
 func _get_or_create_label(id: String) -> Label:
-	var l = _labels_reuse.get(id) as Label
+	var l = _labels_reuse.get(id)
 	if (l == null):
 		l = Label.new()
 		l.name = id
@@ -27,7 +42,20 @@ func _get_or_create_label(id: String) -> Label:
 		_labels_reuse.erase(id)
 	_labels[id] = l
 	return l
-	
+
+func _get_or_create_button(id: String) -> Button:
+	var b = _buttons_reuse.get(id)
+	if (b == null):
+		b = Button.new()
+		b.name = id
+		b.pressed.connect(func(): _buttons_clicked[id] = true)
+		b.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+		add_child(b)
+		print("Created button ", b, " with id ", id, " added att path ", get_path_to(b))
+	else:
+		_buttons_reuse.erase(id)
+	_buttons[id] = b
+	return b
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -45,3 +73,15 @@ func _process(delta):
 	_labels = ll
 	_labels.clear()
 	_labels_next_seq_id = 0
+
+	for b in _buttons_reuse.values():
+		b.queue_free()
+	var bb = _buttons_reuse
+	_buttons_reuse = _buttons
+	_buttons = bb
+	_buttons.clear()
+	_buttons_next_seq_id = 0
+	var bc = _buttons_clicked_prev
+	_buttons_clicked_prev = _buttons_clicked
+	_buttons_clicked = bc
+	_buttons_clicked.clear()
--- src/spinner.gd
+++ src/spinner.gd
@@ -1,5 +1,6 @@
 extends MeshInstance3D
 
+var _reverse_direction:bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,9 +9,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_y(delta)
+	var rotation = delta
+	if (_reverse_direction):
+		rotation = rotation * -1
+	rotate_y(rotation)
 	
 	var deg_y:float = self.rotation_degrees.y
 	DevGUI.label(str("y: ", deg_y))
 	if (deg_y < 0):
 		DevGUI.label(str("The value (", deg_y, ") is negative"))
+	
+	if (DevGUI.button("Switch direction")):
+		_reverse_direction = !_reverse_direction
